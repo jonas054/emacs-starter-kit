d5678fbb490bd5cbd2881e157523d3eff2918a4d
Jonas Arvidsson <jonas054@gmail.com>
2012-01-30 14:25:20 +0100

    Cleaned up.

--- jonas.el
+++ jonas.el
@@ -27,32 +27,24 @@
 (global-set-key [(meta up)] 'scroll-down-1) 
 (global-set-key [(meta down)]   'scroll-up-1)
 
-(define-key global-map [f1]   'next-error)
-
 (define-key global-map [dead-tilde] "~")
 (define-key global-map [S-dead-circumflex] "^")
 (define-key global-map [S-dead-grave] "`")
 
+(define-key global-map [f1]   'next-error)
 (global-set-key [(shift f1)]  'previous-error)
 (define-key global-map [f3]   'previous-error) ; in some XEmacs versions, shift in not recognized
 
 (define-key global-map [f2]   'call-last-kbd-macro)
-;(define-key global-map [f5]   'occur)            ;; Search within buffer
 (define-key global-map [f6]   'other-window)     ;; Jump to other window
 (define-key global-map [f7]   'repeat-complex-command)
 (define-key global-map [f9]   'manual-entry)     ;; man <command>  
-(define-key global-map [f10]  'cleanuptabs-buffer)
 (define-key global-map [f11]  'grep)
 (define-key global-map [f12]  'compile)
 (global-set-key [(control f11)]   'isearch-toggle-case-fold)
 (global-set-key (kbd "C-.") 'dabbrev-expand) ;; M-/ takes 3 keys - too inconvenient!
 (global-set-key (kbd "C-#") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-x C-b") 'electric-buffer-list)
-(set-variable 'compile-command "rake ")
-(set-variable 'compile-command "mlgrep -S ")
-
-(global-set-key [(shift f1)]  'previous-error)
-
 
 (set-variable 'compile-command "rake ")
 (set-variable 'grep-command "mlgrep -S ")
